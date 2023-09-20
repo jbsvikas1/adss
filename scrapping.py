7bc0ef3b914bcdf82fb8cbb7dee1546ef48388a0
vikas <vv439088@gmail.com>
2023-09-20 15:51:12 +0530

    changes

--- scrapping.py
+++ scrapping.py
@@ -39,3 +39,45 @@ soup1 = BeautifulSoup(page.content, "html.parser")
 print(soup1)
 
 
+from bs4 import BeautifulSoup
+import requests
+import smtplib
+import time
+import datetime
+# Connect to Website and pull in data
+
+URL = 'https://www.amazon.com/Funny-Data-Systems-Business-Analyst/dp/B07FNW9FGJ/ref=sr_1_3?dchild=1&keywords=data%2Banalyst%2Btshirt&qid=1626655184&sr=8-3&customId=B0752XJYNL&th=1'
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+
+page = requests.get(URL, headers=headers)
+
+
+soup1 = BeautifulSoup(page.content, "html.parser")
+
+
+print(soup1)
+
+
+
+from bs4 import BeautifulSoup
+import requests
+import smtplib
+import time
+import datetime
+# Connect to Website and pull in data
+
+URL = 'https://www.amazon.com/Funny-Data-Systems-Business-Analyst/dp/B07FNW9FGJ/ref=sr_1_3?dchild=1&keywords=data%2Banalyst%2Btshirt&qid=1626655184&sr=8-3&customId=B0752XJYNL&th=1'
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+
+page = requests.get(URL, headers=headers)
+
+
+soup1 = BeautifulSoup(page.content, "html.parser")
+
+
+print(soup1)
+
+
+i
